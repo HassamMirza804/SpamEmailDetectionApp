d07c0af371c46048b55eb3de2b17129cd2ef48d1
HassamMirza <147750316+HassamMirza804@users.noreply.github.com>
2024-10-18 05:01:28 -0700

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-# mlapp/urls.py
-
-from django.urls import path
-from . import views  # Ensure this line is present
-
-urlpatterns = [
-    path('', views.home, name='home'),  # This should match the defined view
-    path('check-spam/', views.SomeView.as_view(), name='some_view'),  # Add your other views here
-]
